2b63124fd7f1ba485b59ec24e5330446557d6986
SpencerApel <spencerapel@gmail.com>
2021-02-19 12:50:15 -0600

    Comments added to image.c and image.h

--- image.c
+++ image.c
@@ -10,13 +10,13 @@ Imlib_Image *im_broken;
 
 void img_init(img_t *img, win_t *win)
 {
-
+    // set im_broken to broken image from icon.h
     im_broken = imlib_create_image_using_data(32, 32, icon_broken);
 
     if (img)
-        img->im = NULL;
+        img->im = NULL; // set image var to null
 
-    if (win)
+    if (win) // if window is initialized, set imlib contexts
     {
         imlib_context_set_display(win->env.dpy);
         imlib_context_set_visual(win->env.vis);
@@ -26,6 +26,7 @@ void img_init(img_t *img, win_t *win)
 
 void img_free(img_t* img)
 {
+    // set context to broken image and free image
     imlib_context_set_image(im_broken);
     imlib_free_image();
 }
@@ -34,15 +35,15 @@ int img_check(const char *filename)
 {
     Imlib_Image *im;
 
-    if (!filename)
+    if (!filename) // return if no filename given
         return 0;
 
+    // test for access of file, and file is loaded into im var
     if (!access(filename, F_OK) && (im = imlib_load_image(filename)))
     {
-        imlib_context_set_image(im);
-        imlib_image_set_changes_on_disk();
-        imlib_free_image();
-        return 1;
+        imlib_context_set_image(im); // set the image context were working with
+        imlib_free_image(); // and free the image
+        return 1; // success
     }
     else
     {
@@ -53,29 +54,31 @@ int img_check(const char *filename)
 
 int img_load(img_t *img, const char *filename)
 {
-    if (!img || !filename)
+    if (!img || !filename) // return if no img or filename
         return 0;
 
+    // test for access of file, and file is loaded into im var
+    // if so set the image context
     if (!access(filename, F_OK) && (img->im = imlib_load_image(filename)))
-    {
         imlib_context_set_image(img->im);
-        imlib_image_set_changes_on_disk();
-    }
     else
     {
+        // If not accessed, warm user and set broken image
         warn("could not open file: %s", filename);
         imlib_context_set_image(im_broken);
         imlib_context_set_anti_alias(0);
     }
-
+    // set image width and height
     img->w = imlib_image_get_width();
     img->h = imlib_image_get_height();
 
-    return 1;
+    return 1; // success
 }
 
 void img_close(img_t *img)
 {
+    // If image is loaded, set the context to work with,
+    // free the image and set to NULL
     if (img && img->im)
     {
         imlib_context_set_image(img->im);
--- image.h
+++ image.h
@@ -15,11 +15,25 @@ typedef struct img_s
     int h;
 } img_t;
 
+/* Initalizes broken image icon, image to null
+and imlib contexts if window is created */
 void img_init(img_t*, win_t*);
+
+/* Sets image to broken image and free image */
 void img_free(img_t*);
 
+/* Checks if image can be accessed
+if so, set image and free */
 int img_check(const char*);
+
+/* If image can be accessed and loaded, set image,
+get width and height. If not accessed, warm user
+and set broken image (does not get freed like
+                      in img_check) */
 int img_load(img_t*, const char*);
+
+/* If image is loaded, set the context to work with,
+free the image and set to NULL*/
 void img_close(img_t*);
 
 #endif /* IMAGE_H */
