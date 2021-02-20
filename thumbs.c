c68b5d00933854a4c04c3eaaf69f985a4a9de9d9
SpencerApel <spencerapel@gmail.com>
2021-02-20 17:55:38 -0600

    Comments added to thumbs.h and thumbs.c

--- thumbs.c
+++ thumbs.c
@@ -15,22 +15,26 @@ void tns_init(tns_t *tns, int cnt)
     if (!tns)
         return;
 
-    tns->cnt = tns->first = tns->sel = 0;
+    tns->cnt = tns->first = tns->sel = 0; // init cnt, first, sel to 0
+
+    // allocate and set memory
     tns->thumbs = (thumb_t*) s_malloc(cnt * sizeof(thumb_t));
     memset(tns->thumbs, 0, cnt * sizeof(thumb_t));
-    tns->dirty = 0;
+
+    tns->dirty = 0; // set to 0, meaning no refresh will be needed
 }
 
 void tns_free(tns_t *tns, win_t *win)
 {
-    int i;
-
     if (!tns || !tns->thumbs)
         return;
 
-    for (i = 0; i < tns->cnt; ++i)
+    // for each thumbnail in thumbnail mode, cycle
+    // through the list and free the pixmap
+    for (int i = 0; i < tns->cnt; ++i)
         win_free_pixmap(win, tns->thumbs[i].pm);
 
+    // free thumbnail images and set to null
     free(tns->thumbs);
     tns->thumbs = NULL;
 }
@@ -42,34 +46,41 @@ void tns_load(tns_t *tns, win_t *win, const char *filename)
     thumb_t *t;
     Imlib_Image *im;
 
+    // return if no thumbnails, window, or files
     if (!tns || !win || !filename)
         return;
 
+    // if file loaded as image, set image context with image
+    // else set image context with broken image
     if ((im = imlib_load_image(filename)))
         imlib_context_set_image(im);
     else
         imlib_context_set_image(im_broken);
 
+    // set w, h using imlib functions
     w = imlib_image_get_width();
     h = imlib_image_get_height();
+
     zw = (float) THUMB_SIZE / (float) w;
     zh = (float) THUMB_SIZE / (float) h;
     z = MIN(zw, zh);
     if (!im && z > 1.0)
         z = 1.0;
 
-    t = &tns->thumbs[tns->cnt++];
-    t->w = z * w;
-    t->h = z * h;
+    t = &tns->thumbs[tns->cnt++]; // add thumbnail image list
+    t->w = z * w; // set new width
+    t->h = z * h; // set new height
 
+    // create pixmap w, h of thumbnail, and set drawable
     t->pm = win_create_pixmap(win, t->w, t->h);
     imlib_context_set_drawable(t->pm);
     imlib_context_set_anti_alias(1);
-    imlib_render_image_part_on_drawable_at_size(0, 0, w, h,
-            0, 0, t->w, t->h);
-    tns->dirty = 1;
 
-    if (im)
+    // render image in grid in thumbnail mode
+    imlib_render_image_part_on_drawable_at_size(0, 0, w, h, 0, 0, t->w, t->h);
+    tns->dirty = 1; // set to one to cause window to refresh
+
+    if (im) // after image placed on screen, free image
         imlib_free_image();
 }
 
@@ -77,32 +88,40 @@ void tns_check_view(tns_t *tns, Bool scrolled)
 {
     int r;
 
-    if (!tns)
+    if (!tns) // if no thumbnails, return
         return;
 
     tns->first -= tns->first % tns->cols;
     r = tns->sel % tns->cols;
 
+    /* if the user scrolls with the mouse wheel,
+    first move the selection (border around the thumbnal)
+    then refresh the screen and update the thumbnails...
+    simulating a scrolling function */
     if (scrolled)
     {
         /* move selection into visible area */
+        // if selection is past the last photo in the bottom right corner
         if (tns->sel >= tns->first + tns->cols * tns->rows)
+            // move selection border down a row, all the way to the first col
             tns->sel = tns->first + r + tns->cols * (tns->rows - 1);
-        else if (tns->sel < tns->first)
-            tns->sel = tns->first + r;
+        else if (tns->sel < tns->first) // if selection is beofre the first thumbnail on screen
+            tns->sel = tns->first + r; // move sel border up a row, to the last col
     }
     else
     {
         /* scroll to selection */
+        // if selection is past the last photo in the bottom right corner
         if (tns->first + tns->cols * tns->rows <= tns->sel)
         {
+            // move down a row, and remove the top row
             tns->first = tns->sel - r - tns->cols * (tns->rows - 1);
-            tns->dirty = 1;
+            tns->dirty = 1; // set to one to refresh
         }
-        else if (tns->first > tns->sel)
-        {
-            tns->first = tns->sel - r;
-            tns->dirty = 1;
+        else if (tns->first > tns->sel) // if first thumbnail is greater than selection
+        {                               // aka selection is on image above what is shown
+            tns->first = tns->sel - r; // move to row that selection is on
+            tns->dirty = 1; // set to one to refresh
         }
     }
 }
@@ -112,14 +131,19 @@ void tns_render(tns_t *tns, win_t *win)
     int i, cnt, r, x, y;
     thumb_t *t;
 
+    // no thumbnail, window or doesnt need refreshed
     if (!tns || !tns->dirty || !win)
         return;
 
-    win_clear(win);
+    win_clear(win); // clear the thumbnail grid
 
+    // calculate number of rows, cols
     tns->cols = MAX(1, win->w / thumb_dim);
     tns->rows = MAX(1, win->h / thumb_dim);
 
+    /* if amount of thumbnails < number of spaces made by rows, cols
+    then space was correctly made and set vars
+    else call tns_check_view to fix/reconfigure the window and grid */
     if (tns->cnt < tns->cols * tns->rows)
     {
         tns->first = 0;
@@ -135,29 +159,31 @@ void tns_render(tns_t *tns, win_t *win)
             cnt -= r % tns->cols;
     }
 
+    // set x, y coords
     r = cnt % tns->cols ? 1 : 0;
     tns->x = x = (win->w - MIN(cnt, tns->cols) * thumb_dim) / 2 + 5;
     tns->y = y = (win->h - (cnt / tns->cols + r) * thumb_dim) / 2 + 5;
 
+    // for each thumbnail, calculate their x,y coord for each grid
+    // draw the pixmap with those coords
     for (i = 0; i < cnt; ++i)
     {
         t = &tns->thumbs[tns->first + i];
         t->x = x + (THUMB_SIZE - t->w) / 2;
         t->y = y + (THUMB_SIZE - t->h) / 2;
         win_draw_pixmap(win, t->pm, t->x, t->y, t->w, t->h);
+        // calculations for end of cols, placing in the next row, first col
         if ((i + 1) % tns->cols == 0)
         {
             x = tns->x;
             y += thumb_dim;
         }
         else
-        {
             x += thumb_dim;
-        }
     }
 
-    tns->dirty = 0;
-    tns_highlight(tns, win, tns->sel, True);
+    tns->dirty = 0; // set 0, no refresh needed
+    tns_highlight(tns, win, tns->sel, True); // highlight the first or previous (before re-render) thumbnail
 }
 
 void tns_highlight(tns_t *tns, win_t *win, int n, Bool hl)
@@ -167,13 +193,14 @@ void tns_highlight(tns_t *tns, win_t *win, int n, Bool hl)
     if (!tns || !win)
         return;
 
+    // draws rectangle around selected thumbnail
     if (n >= 0 && n < tns->cnt)
     {
         t = &tns->thumbs[n];
         win_draw_rect(win, t->x - 2, t->y - 2, t->w + 4, t->h + 4, hl);
     }
 
-    win_draw(win);
+    win_draw(win); // call function from window.c
 }
 
 int tns_move_selection(tns_t *tns, win_t *win, tnsdir_t dir)
@@ -183,8 +210,10 @@ int tns_move_selection(tns_t *tns, win_t *win, tnsdir_t dir)
     if (!tns || !win)
         return 0;
 
-    old = tns->sel;
+    old = tns->sel; // set old selection
 
+    // calculations to move the new selection to
+    // the correct thumbnail
     switch (dir)
     {
     case TNS_LEFT:
@@ -205,6 +234,8 @@ int tns_move_selection(tns_t *tns, win_t *win, tnsdir_t dir)
         break;
     }
 
+    // once selection has changed, call highlight function
+    // to draw border and refresh window
     if (tns->sel != old)
     {
         tns_highlight(tns, win, old, False);
@@ -225,17 +256,21 @@ int tns_scroll(tns_t *tns, tnsdir_t dir)
 
     old = tns->first;
 
+    // if scrolled down
     if (dir == TNS_DOWN && tns->first + tns->cols * tns->rows < tns->cnt)
     {
+        // update cols, and re-calculate view
         tns->first += tns->cols;
         tns_check_view(tns, True);
-        tns->dirty = 1;
+        tns->dirty = 1; // calls for refresh
     }
+    // if scrolled up
     else if (dir == TNS_UP && tns->first >= tns->cols)
     {
+        // update cols, and re-calculate view
         tns->first -= tns->cols;
         tns_check_view(tns, True);
-        tns->dirty = 1;
+        tns->dirty = 1; // calls for refresh
     }
 
     return tns->first != old;
--- thumbs.h
+++ thumbs.h
@@ -11,6 +11,7 @@ typedef enum tnsdir_e
     TNS_DOWN
 } tnsdir_t;
 
+/* struct for each individual thumbnail */
 typedef struct thumb_s
 {
     Pixmap pm;
@@ -20,6 +21,7 @@ typedef struct thumb_s
     int h;
 } thumb_t;
 
+/* struct for the thumbnail grid */
 typedef struct tns_s
 {
     thumb_t *thumbs;
@@ -33,15 +35,31 @@ typedef struct tns_s
     unsigned char dirty;
 } tns_t;
 
+/* Initialize variables, allocate and set memory */
 void tns_init(tns_t*, int);
+
+/* Free thumbnail pixmap and thumbnail images
+also set thumbnails to null */
 void tns_free(tns_t*, win_t*);
 
+/* Sets image context, calculates width and height
+creates pixmap, renders image in pixmap in a grid */
 void tns_load(tns_t*, win_t*, const char*);
 
+/* Clears window, calculates x, y cords, draws
+pixmap of image on grid and runs tns_highlight */
 void tns_render(tns_t*, win_t*);
+
+/* Draws rectangle around image that is
+currently "selected" */
 void tns_highlight(tns_t*, win_t*, int, Bool);
 
+/* Moves selection, up, down, left, right when
+arrow keys or vim bindings are clicked */
 int tns_move_selection(tns_t*, win_t*, tnsdir_t);
+
+/* Calculates what to do when user scrolls mouse wheel
+and then calls a function re-renders window */
 int tns_scroll(tns_t*, tnsdir_t);
 
 #endif /* THUMBS_H */
