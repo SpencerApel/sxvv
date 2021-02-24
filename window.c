a7594dbd5e47de64d7cbd315102c5c212d51f3ef
SpencerApel <spencerapel@gmail.com>
2021-02-24 16:24:22 -0600

    Comments added to window.h and window.c

--- window.c
+++ window.c
@@ -12,7 +12,7 @@ static Cursor carrow;
 //static Cursor cnone;
 static GC gc;
 
-Atom wm_delete_win;
+Atom wm_delete_win; // allow window manager to close window
 
 void win_open(win_t *win)
 {
@@ -22,13 +22,15 @@ void win_open(win_t *win)
     XGCValues gcval;
     int gmask;
 
-    if (!win)
+    if (!win) // if window not created correctly, return
         return;
 
     e = &win->env;
+    // error handling
     if (!(e->dpy = XOpenDisplay(NULL)))
         die("could not open display");
 
+    // Initialize window w, h, display, visual, colors
     e->scr = DefaultScreen(e->dpy);
     e->scrw = DisplayWidth(e->dpy, e->scr);
     e->scrh = DisplayHeight(e->dpy, e->scr);
@@ -36,11 +38,14 @@ void win_open(win_t *win)
     e->cmap = DefaultColormap(e->dpy, e->scr);
     e->depth = DefaultDepth(e->dpy, e->scr);
 
+    // set background color
     if (XAllocNamedColor(e->dpy, DefaultColormap(e->dpy, e->scr), BG_COLOR,
                          &col, &col))
         win->bgcol = col.pixel;
     else
         die("could not allocate color: %s", BG_COLOR);
+
+    // set selection color
     if (XAllocNamedColor(e->dpy, DefaultColormap(e->dpy, e->scr), SEL_COLOR,
                          &col, &col))
         win->selcol = col.pixel;
@@ -70,31 +75,38 @@ void win_open(win_t *win)
     else if (gmask & YNegative)
         win->y += e->scrh - win->h;
 
+    // Create the X window
     win->xwin = XCreateWindow(e->dpy, RootWindow(e->dpy, e->scr),
                               win->x, win->y, win->w, win->h, 0,
                               e->depth, InputOutput, e->vis, 0, None);
-    if (win->xwin == None)
+    if (win->xwin == None) // error handling
         die("could not create window");
 
+    // Initialize user input
     XSelectInput(e->dpy, win->xwin, StructureNotifyMask | KeyPressMask |
                  ButtonPressMask | ButtonReleaseMask | PointerMotionMask);
 
+    // Declare type of pointer, only arrow pointer currently
     carrow = XCreateFontCursor(e->dpy, XC_left_ptr);
 
+    // error handling for color black
     if (!XAllocNamedColor(e->dpy, DefaultColormap(e->dpy, e->scr), "black",
                           &col, &col))
         die("could not allocate color: black");
 
+    // setting and creating graphic content
     gcval.line_width = 2;
     gc = XCreateGC(e->dpy, win->xwin, GCLineWidth, &gcval);
 
+    // set name for classhint
     classhint.res_name = "sxvv";
     classhint.res_class = "sxvv";
     XSetClassHint(e->dpy, win->xwin, &classhint);
 
-    XMapWindow(e->dpy, win->xwin);
+    XMapWindow(e->dpy, win->xwin); // map window to display
     XFlush(e->dpy);
 
+    // kill program in deleted by window manager (not by esc or q)
     wm_delete_win = XInternAtom(e->dpy, "WM_DELETE_WINDOW", False);
     XSetWMProtocols(e->dpy, win->xwin, &wm_delete_win, 1);
 }
@@ -104,6 +116,8 @@ void win_close(win_t *win)
     if (!win)
         return;
 
+    // free cursor, graphic content, window and
+    // closes the program nicely
     XFreeCursor(win->env.dpy, carrow);
 
     XFreeGC(win->env.dpy, gc);
@@ -119,8 +133,13 @@ int win_configure(win_t *win, XConfigureEvent *c)
     if (!win)
         return 0;
 
+    // set changed if the widths or heights are not same value
+    // if they are the same value, the setting of values below
+    // will not matter because they are the same value
     changed = win->w != c->width || win->h != c->height;
 
+    // change the x, y coords, width and height and
+    // order width and return changed (true if needed changed)
     win->x = c->x;
     win->y = c->y;
     win->w = c->width;
@@ -130,24 +149,29 @@ int win_configure(win_t *win, XConfigureEvent *c)
     return changed;
 }
 
-int win_moveresize(win_t *win, int x, int y, unsigned int w, unsigned int h)
+int win_moveresize(win_t *win, int x, int y, unsigned int w, unsigned int h) // currently not used, may fix resize issue...
 {
     if (!win)
         return 0;
 
+    // take max value for x, y. default 0
+    // set width and height
     x = MAX(0, x);
     y = MAX(0, y);
     w = MIN(w, win->env.scrw - 2 * win->bw);
     h = MIN(h, win->env.scrh - 2 * win->bw);
 
+    // if values are the same (no change) just return
     if (win->x == x && win->y == y && win->w == w && win->h == h)
         return 0;
 
+    // set new window values
     win->x = x;
     win->y = y;
     win->w = w;
     win->h = h;
 
+    // call X function to resize the window
     XMoveResizeWindow(win->env.dpy, win->xwin, win->x, win->y, win->w, win->h);
 
     return 1;
@@ -157,12 +181,13 @@ Pixmap win_create_pixmap(win_t *win, int w, int h)
 {
     if (!win)
         return 0;
-
+    // creates pixmap if window is created
     return XCreatePixmap(win->env.dpy, win->xwin, w, h, win->env.depth);
 }
 
 void win_free_pixmap(win_t *win, Pixmap pm)
 {
+    // if window and pixmap are created, free the pixmap
     if (win && pm)
         XFreePixmap(win->env.dpy, pm);
 }
@@ -178,17 +203,21 @@ void win_clear(win_t *win)
     e = &win->env;
     gcval.foreground = win->fullscreen ? BlackPixel(e->dpy, e->scr) :
                        win->bgcol;
+
+    // free the pixmap and create the new pixmap
     if (win->pm)
         XFreePixmap(e->dpy, win->pm);
     win->pm = XCreatePixmap(e->dpy, win->xwin, e->scrw, e->scrh, e->depth);
 
+    // change graphic content with newly set window env and gc
     XChangeGC(e->dpy, gc, GCForeground, &gcval);
+
     XFillRectangle(e->dpy, win->pm, gc, 0, 0, e->scrw, e->scrh);
 }
 
 void win_draw_pixmap(win_t *win, Pixmap pm, int x, int y, int w, int h)
 {
-    if (win)
+    if (win) // copy other "rectangle pixmap" and draw next one
         XCopyArea(win->env.dpy, pm, win->pm, gc, 0, 0, w, h, x, y);
 }
 
@@ -207,6 +236,7 @@ void win_draw_rect(win_t *win, int x, int y, int w, int h, Bool sel)
     else
         gcval.foreground = win->fullscreen ? BlackPixel(e->dpy, e->scr) :
                            win->bgcol;
+    // update graphcis content and draw thumbnail rectangle
     XChangeGC(e->dpy, gc, GCForeground, &gcval);
     XDrawRectangle(e->dpy, win->pm, gc, x, y, w, h);
 }
@@ -216,6 +246,7 @@ void win_draw(win_t *win)
     if (!win)
         return;
 
+    // sets windows background pixmap and clears window
     XSetWindowBackgroundPixmap(win->env.dpy, win->xwin, win->pm);
     XClearWindow(win->env.dpy, win->xwin);
 }
@@ -225,5 +256,6 @@ void win_set_cursor(win_t *win, win_cur_t cursor)
     if (!win)
         return;
 
+    // defines cursor as arrow
     XDefineCursor(win->env.dpy, win->xwin, carrow);
 }
--- window.h
+++ window.h
@@ -3,11 +3,14 @@
 
 #include <X11/Xlib.h>
 
+/* Window cursor icon, only arrow, no other for now */
 typedef enum win_cur_e
 {
     CURSOR_ARROW = 0,
 } win_cur_t;
 
+/* Window environment struct dealing with
+windows display, visuals, colors and size */
 typedef struct win_env_s
 {
     Display *dpy;
@@ -18,6 +21,8 @@ typedef struct win_env_s
     int depth;
 } win_env_t;
 
+/* Struct for the window dealing with colors,
+pixmap, coordinates and borders */
 typedef struct win_s
 {
     Window xwin;
@@ -38,20 +43,40 @@ typedef struct win_s
 
 extern Atom wm_delete_win; //allows program to be closed/exited by WM
 
+/* Creates window and everything entailing the window sets
+user input, cursor, graphic content, classhint, map window to display */
 void win_open(win_t*);
+
+/* Frees necessary content and closes window nicely */
 void win_close(win_t*);
 
+/* Sets the x, y coords, width and height, order width
+and return changed */
 int win_configure(win_t*, XConfigureEvent*);
+
+/* Updates x, y coordinates and width, height, then resizes window */
 int win_moveresize(win_t*, int, int, unsigned int, unsigned int);
 
+/* Creates pixmap is window is created */
 Pixmap win_create_pixmap(win_t*, int, int);
+
+/* Fress pixmap */
 void win_free_pixmap(win_t*, Pixmap);
 
+/* Deletes and creates a new pixmap and
+updates the graphics content */
 void win_clear(win_t*);
+
+/* Draws the pixmap for the grid */
 void win_draw_pixmap(win_t*, Pixmap, int, int, int, int);
+
+/* Update graphcis content and draw thumbnail rectangle */
 void win_draw_rect(win_t*, int, int, int, int, Bool);
+
+/* Sets windows background pixmap and clears window */
 void win_draw(win_t*);
 
+/* Defines cursor as arrow */
 void win_set_cursor(win_t*, win_cur_t);
 
 #endif /* WINDOW_H */
